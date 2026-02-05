2d56449626d5fbc19ae9028fc738d5c09cc10c33
Jashwanth Diddukoori <150791464+jashwanth037@users.noreply.github.com>
2026-02-05 18:13:06 +0530

    Update loggger.middleware.js

---
+++ complaints-api/middleware/loggger.middleware.js
@@ -0,0 +1,11 @@
+const logger = (req, res, next) => {
+    const method = req.method;
+    const url = req.url;
+    const timestamp = new Date().toISOString();
+
+    console.log(`[${timestamp}] ${method} ${url}`);
+
+    next();
+}
+
+export default logger;
