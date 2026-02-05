b92bb062df4fbd2e5178d042ffe89c15a679ec17
Jashwanth Diddukoori <150791464+jashwanth037@users.noreply.github.com>
2026-02-05 18:13:39 +0530

    Update complaint.routes.js

--- complaints-api/routes/complaint.routes.js
+++ complaints-api/routes/complaint.routes.js
@@ -1,13 +1,13 @@
 import express from 'express';
+import { displayComplaints, addComplaints, resolveComplaint, deleteComplaint, getComplaintById } from '../controllers/compalint.controller.js';
+import authMiddleware from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
-const complaints = []
-
-router.get("/", (req, res) => {
-    res.send(complaints);
-});
-
-router.push
+router.get("/", displayComplaints);
+router.post("/", addComplaints);
+router.get("/:id", getComplaintById);
+router.put("/:id/resolve", authMiddleware, resolveComplaint);
+router.delete("/:id", authMiddleware, deleteComplaint);
 
 export default router;
